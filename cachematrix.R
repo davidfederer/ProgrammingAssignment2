4b532e9dc4888fe141c9003af6e0f297dcb05e30
davidfederer <david.j.federer@gmail.com>
2014-10-27 10:42:21 +1100

    final submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below allow the inverse of a non-singular matrix to be cached and looked
+## up, rather than recomputed which may be time consuming for big matrices
 
-## Write a short comment describing this function
+
+## The makeCacheMatrix function creates/caches the inverse of a non-singular matrix provides the 
+## functionality to get and set either the original and the inverse matrices
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv_mat <- NULL
+  set <- function(y) {
+    x <<- y #sets matrix
+    inv_mat <<- matrix()
+  }
+  get <- function() x #gets matrix
+  setinvmat <- function(inverse) inv_mat <<- inverse
+  getinvmat <- function() inv_mat
+  list(set = set, get = get,
+       setinvmat = setinvmat,
+       getinvmat = getinvmat)
+  
 }
 
 
-## Write a short comment describing this function
+## The cachesolve function calculates the inverse of a non-singular matrix created with the above function. 
+## However, it first checks to see if the inverse matrix has already been calculated. 
+## If so, it gets the inverse matrix from the cache and skips the computation. 
+## Otherwise, it calculates the inverse matrix of the data and sets the inverse matrix in the cache via the setinvmat function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv_mat <- x$getinvmat()
+  if(!is.nan(det(inv_mat))) {
+    message("getting cached data")
+    return(inv_mat)
+  }
+  mat <- x$get()
+  inv_mat <- solve(mat, ...)
+  x$setinvmat(inv_mat)
+  inv_mat 
 }
